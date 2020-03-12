e3b7af3d13e78efa559551312e48d299d10f2911
Faraz Hussain <faraz.hussain@epsilon.com>
2020-03-12 11:01:40 +0530

    Commit Five

--- Exceptions Demo/src/com/epsilon/programs/GetQuotient.java
+++ Exceptions Demo/src/com/epsilon/programs/GetQuotient.java
@@ -8,14 +8,21 @@ public class GetQuotient {
 		try {
 			String a1 = args[0];
 			String a2 = args[1];
-
 			int n = Integer.parseInt(a1);// by default, the base is 10
 			int d = Integer.parseInt(a2);
 			int q = n / d;
 			System.out.println("Quotient of " + n + " / " + d + " = " + q);
 
-		} catch (Exception e) {
-			System.out.println("There was a problem: " + e.getMessage());
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Two Inputs Expected!" + e.getMessage());
+		} catch (NumberFormatException e) {
+			System.err.println("Only Integer Values are allowed!");
+			// return;
+			// System.exit(12);
+		} catch (Exception e) {// fallback/default:must always be the last one
+			System.err.println("Two inputs Expected!");
+		} finally {
+			System.out.println("This works in most cases!");// Avoid using Finally
 		}
 
 		System.out.println("End of the main !");
